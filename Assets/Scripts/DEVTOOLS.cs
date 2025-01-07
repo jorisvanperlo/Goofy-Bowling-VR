ca37ef2bc556688806469ea56b04b12e7284057c
jorisvanperlo <159013746+jorisvanperlo@users.noreply.github.com>
2025-01-07 14:06:32 +0100

    glijbaan toegevoegd.

--- Assets/Scripts/DEVTOOLS.cs
+++ Assets/Scripts/DEVTOOLS.cs
@@ -14,7 +14,7 @@ public class DEVTOOLS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.P))
+        if (Input.GetKeyUp(KeyCode.D))
         {
             devPowerUps.SetActive(true);
         }
